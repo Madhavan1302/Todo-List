7d1f35513562ec0969559fae7e81579fb3184c34
Madhavan1302 <madhavan0923@gmail.com>
2025-06-16 07:22:29 +0100

    UI changed

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,8 @@ function App() {
   
   return (
    <>
-   <div className="app-container p-3 d-flex flex-column justify-content-center align-items-center">
+   <div className='w-100 h-100 cont d-flex justify-content-center align-items-center'>
+   <div className="app-container flex-shrink-0 container-md mx-auto p-3 d-flex flex-column justify-content-center align-items-center " >
     <h3>Todo List</h3>
     <div className="m-2 input-container d-flex justify-content-between">
       <input type="text" className="mx-2 px-3" placeholder='Add Your Task' value={input} onChange={(e)=>{setInput(e.target.value)}}/>
@@ -40,12 +41,13 @@ function App() {
     </div>
     <div className="w-100 todos-container">
       <ul className='w-100'>
-        {todos.map((todo)=>( <li className=' p-2 w-100 my-1 d-flex align-items-center justify-content-between'key={todo.id}>{todo.data}<span><button className='btn btn-success'  onClick={()=>{handleEdit(todo.id)}}>Edit</button><button className='btn btn-danger mx-1' onClick={()=>(handleDelete(todo.id))}>Delete</button></span></li>))}
+        {todos.map((todo)=>( <li className=' p-2 w-100 my-1 d-flex align-items-center justify-content-between'key={todo.id} onDoubleClick={()=>{handleEdit(todo.id)}}>{todo.data}<span><button className='btn btn-danger mx-1' onClick={()=>(handleDelete(todo.id))}>Delete</button></span></li>))}
       </ul>
     </div>
     <div className="copy-right py-2">
       <p>Designed by <a href="https://www.linkedin.com/in/madhavan-p-b75b752b0?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" target="_blank">Madhavan</a></p>
     </div>
+   </div>
    </div>
    </>
   )
